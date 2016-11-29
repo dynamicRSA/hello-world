96132886c2a168eca37484ab2152426faffcbd69
Sven Prinsloo <sven@LAWTrust.co.za>
2016-11-29 23:56:11 +0200

    Initial HelloWorld commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like to (S)tart or (A)bort?");
+            String input = Console.ReadLine();
+            while (input != "A") {
+                if (input == "S")
+                {
+                    Console.WriteLine(".      . .                 .   . ");
+                    Console.WriteLine("|-. .-,| | .-.  . . ..-..-.| .-| ");
+                    Console.WriteLine("' '-`'-'-'-`-'   ` ` `-''  '-`-'-");
+                   // Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.WriteLine("Would you like to (S)tart or (A)bort again?");
+                    input = Console.ReadLine();
+                }
+                else if ((input != "A") || (input != "S"))
+                {
+                    Console.WriteLine("Invalid input, re-enter 'A' or 'S'");
+                    Console.ReadLine();
+                }
+                }
+                Environment.Exit(0);
+            }
+        }
+    }
+
